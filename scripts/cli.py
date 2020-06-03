e8f9a6c163045a623cfab083aa1cda41b82e2b1e
Mohcine EL KASSIB <elkassib.mohcine@gmail.com>
2020-06-04 00:10:59 +0100

    Add documentation to CLI related files

--- scripts/cli.py
+++ scripts/cli.py
@@ -5,12 +5,15 @@ from validate import validate, extract
 import re as regex
 import sys
 
+# Define aliases for readability
 Talk = Dict[str, str]
 Talks = List[Talk]
 User = str
 
 
 def print_talk(talk: Talk) -> None:
+    """Display information about the talk."""
+
     print(f"  {talk['title'].center(50)}\n")
     print(f"  {talk['type']} by {talk['talkOwner']}")
     print(f"  -> {talk['description']}")
@@ -21,8 +24,13 @@ def print_talk(talk: Talk) -> None:
 
 
 def fetch_tags(db: Dict[str, Dict[User, Talks]], args: Namespace) -> None:
+    """Display available tags in database."""
+
     if args.list:
+        # find tags / find tags -ls / find tags --list
         print('Available tags are:')
+
+        # Print tags in columns
         for chunk in split_list(db['tags'], 3):
             for tag in chunk:
                 print(f" # {tag:20}", end="")
@@ -30,29 +38,40 @@ def fetch_tags(db: Dict[str, Dict[User, Talks]], args: Namespace) -> None:
 
 
 def fetch_talks(db: Dict[str, Dict[User, Talks]], args: Namespace) -> None:
+    """Fetch talks from database and display them."""
+
     for user, talks in db['talks'].items():
         for talk in talks:
+            # find talks --title .. --user .. --tags ..
             if args.tags and args.user:
                 if regex.search(args.title, talk['title'])\
                   and has_common_tags(talk['tags'], args.tags)\
                   and args.user == user:
                     print_talk(talk)
+            # find talks --title .. --user ..
             elif args.user and args.tags is None:
                 if regex.search(args.title, talk['title'])\
                   and args.user == user:
                     print_talk(talk)
+            # find talks --title .. --tags ..
             elif args.tags and args.user is None:
                 if regex.search(args.title, talk['title'])\
                    and has_common_tags(talk['tags'], args.tags):
                     print_talk(talk)
             else:
+                # find talks --title ..
                 if regex.search(args.title, talk['title']):
                     print_talk(talk)
 
 
 def fetch_users(db: Dict[str, Dict[User, Talks]], args: Namespace) -> None:
+    """Fetch users from database and display their usernames."""
+
     if args.list:
+        # find users / find users -ls / find users --list
         print('Available users are:')
+
+        # Print usernames in columns
         for chunk in split_list(list(db['talks'].keys()), 3):
             for user in chunk:
                 print(f" # {user:20}", end="")
@@ -60,6 +79,9 @@ def fetch_users(db: Dict[str, Dict[User, Talks]], args: Namespace) -> None:
 
 
 def validate_note(db: Dict[str, Dict[User, Talks]], args: Namespace) -> None:
+    """Verify if the note contains required metadata."""
+
+    # validate --file .. .. ..
     for filename in args.file:
         with filename as file:
             if not validate(extract(file)):
--- scripts/collectalk.py
+++ scripts/collectalk.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser, FileType
 from helpers import read_json
 import cli
 
-
+# Helper messages for CLI
 txt = {
     'prog.desc':
         'Add notes about talks and look for ones you are interested about.',
@@ -28,8 +28,11 @@ parser.add_argument(
     '-v', '--version', action='version', version='%(prog)s version 1.0.0')
 subparsers = parser.add_subparsers()
 
+# Create the command 'find'
 find_parser = subparsers.add_parser('find', help=txt['find'])
 find_subparsers = find_parser.add_subparsers()
+
+# Create the subcommand 'find talks'
 talks_parser = find_subparsers.add_parser('talks', help=txt['talks'])
 talks_parser.add_argument(
     '-t', '--title', default='.*', help=txt['talks.title'], type=str)
@@ -38,21 +41,25 @@ talks_parser.add_argument(
     '--tags', nargs='+', help=txt['talks.tags'], type=str)
 talks_parser.set_defaults(func=cli.fetch_talks)
 
+# Create the subcommand 'find users'
 users_parser = find_subparsers.add_parser('users', help=txt['users'])
 users_parser.add_argument(
     '-ls', '--list', default=True, action='store_true', help=txt['users.list'])
 users_parser.set_defaults(func=cli.fetch_users)
 
+# Create the subcommand 'find tags'
 tags_parser = find_subparsers.add_parser('tags', help=txt['tags'])
 tags_parser.add_argument(
     '-ls', '--list', default=True, action='store_true', help=txt['tags.list'])
 tags_parser.set_defaults(func=cli.fetch_tags)
 
+# Create the command 'validate'
 validate_parser = subparsers.add_parser('validate', help=txt['validate'])
 validate_parser.add_argument(
     '--file', nargs='+', help=txt['validate.file'], type=FileType('r'))
 validate_parser.set_defaults(func=cli.validate_note)
 
+# Create the command 'submit'
 submit_parser = subparsers.add_parser('submit', help=txt['submit'])
 submit_parser.set_defaults(func=cli.submit_note)
 
@@ -64,4 +71,5 @@ if __name__ == '__main__':
     if 'func' in args:
         args.func(db, args)
     else:
+        # if the user calls the script without any command
         parser.print_help()
